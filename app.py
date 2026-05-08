5d656002ee86ecf0c5f23e44947715d81a2adcfd
Kenia48-INGABIRE <niyongabirekenia20@gmail.com>
2026-05-08 16:33:44 +0200

    port integration
    port integration

--- app.py
+++ app.py
@@ -64,5 +64,7 @@ def update(id):
     student = conn.execute("SELECT * FROM students WHERE id=?", (id,)).fetchone()
     conn.close()
     return render_template("update.html", student=student)
-
-app.run(debug=True)
+#app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
